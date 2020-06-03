194591f59d5e684bbc13cb30615ebd41a92d0265
Rudy <rodri833@cougars.csusm.edu>
2020-06-02 20:03:35 -0700

    Initial Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            double x;
+            double y;
+            for (var i = 0; i < 40; i++)
+            {
+                x = rnd.Next(10,200);
+                y = rnd.Next(10,200);
+                Wrap.Children.Add(new Rectangle
+                {
+                    Width = x,
+                    Height = y,
+                    StrokeThickness = 1,
+                    Stroke = new SolidColorBrush(Colors.Black),
+                    Margin = new Thickness(5)
+                });
+            }
+
+            Wrap.Children.Add(new Image {            });// Need to add attributes for adding images. 
+
+
+        }
+
+        private static Random rnd = new Random();
+    }
+}
