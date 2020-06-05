04f02f85bbb4ccf4460c5268c8a16d4eafc8b338
Rudy <JustRudyRodriguez@gmail.com>
2020-06-05 16:13:17 -0700

    Syncing from laptop.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace ReView
 {
@@ -26,6 +27,11 @@ namespace ReView
 
             double x;
             double y;
+            string SourceDirectory = System.IO.Path.GetFullPath(@"..\..\")+"Images";//Gets the path I actually want.
+            var ImagesList = Directory.EnumerateFiles(SourceDirectory);//I can add a search filter here, to only accept specific file types. (SourceDirectory, "*.png") only takes pngs.
+
+            Console.WriteLine(SourceDirectory);
+
             for (var i = 0; i < 40; i++)
             {
                 x = rnd.Next(30,200);
@@ -39,15 +45,17 @@ namespace ReView
                     Margin = new Thickness(5)
                 });
             }
-
-            Wrap.Children.Add(new Image
-            {
-               Source = new BitmapImage(new Uri("C:/Users/rudy_/source/repos/ReView/Images/grzegorz-rakowski-kmdcTVJMrhc-unsplash.jpg"))
-                                        });
+           // Going to use a for each {elem} to put all images from the folder into the app. to see what happens.
+            foreach (string path in ImagesList)
+             {
+                 Wrap.Children.Add(new Image //lol, need to compress this got big fast.
+                 {
+                     Source = new BitmapImage(new Uri(path))
+                 });
+            }
 
 
         }
-
         private static Random rnd = new Random();
     }
 
